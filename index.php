3dd103fbb572c747b37d9626b99d516034d2f2d9
oni <irhamdani@redbuzz.co.id>
2016-09-23 20:03:04 +0700

    test

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Redbuzz\Karyawan\Oni;
+
+$nama = new Oni();
+
+echo $nama->panggil();
---
+++ src/Oni.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Redbuzz\Karyawan;
+
+class Oni {
+	public function panggil() {
+		return "hai hello world";
+	}
+} 
